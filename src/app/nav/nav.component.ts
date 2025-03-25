6366427a6a92a3383622ff66b67b8a4749b47e1b
RAMAROSANDRATANA johary ny aina nirina <joharyny@gmail.com>
2025-03-25 22:56:12 +0100

    add link

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
-
+  @Input() lien:string='';
 }
