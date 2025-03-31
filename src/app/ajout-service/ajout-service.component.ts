472ca8f9b47d8878d0e3ebcf6fd516ce6fc02e6d
RAMAROSANDRATANA johary ny aina nirina <joharyny@gmail.com>
2025-03-31 20:43:51 +0200

    create new component

---
+++ src/app/ajout-service/ajout-service.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ajout-service',
+  imports: [],
+  templateUrl: './ajout-service.component.html',
+  styleUrl: './ajout-service.component.css'
+})
+export class AjoutServiceComponent {
+
+}
